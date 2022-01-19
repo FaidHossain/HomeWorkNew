603de10200297a686ef0ac09cbd1945396d1a231
FaidHossain <fdhssain@gmail.com>
2022-01-18 23:04:25 -0500

    01182022 11:04pm

---
+++ src/hw02/AboutMe.java
@@ -0,0 +1,10 @@
+package hw02;
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		System.out.println("HakimunNahar");
+
+	}
+
+}
---
+++ src/hw04/HomeWork4.java
@@ -0,0 +1,24 @@
+package hw04;
+
+
+public class HomeWork4 {
+	public String myinfo;
+	public String name = (" Faid Hossain");
+	public char gender = 'm';
+	public double heights = 3.4;
+	public float weight = 199f;
+	public long password = 2022l;
+	public int zipcode = 20710;
+	public short carpayment = 900;
+	public boolean resident = false;
+	public byte age = 45;
+
+	public static void main(String[] args) {
+		HomeWork4 homeWork = new HomeWork4();
+
+		System.out.println("My Name: " + homeWork.name + ", My Hights: " + homeWork.heights + ", My Age: "
+				+ homeWork.age + ", My carpayment $ " + homeWork.carpayment + ", My weight:" + homeWork.weight);
+
+	}
+
+}
