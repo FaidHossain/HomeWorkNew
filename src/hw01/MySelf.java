c9fbfd7bf71852b9bd1a6052b5a8016836122827
FaidHossain <fdhssain@gmail.com>
2022-01-18 22:56:10 -0500

    01182022 10:56pm

---
+++ src/hw01/MySelf.java
@@ -0,0 +1,13 @@
+package hw01;
+
+/* Naming convention of java project, packege and class*/
+public class MySelf {
+//Entry point for java
+	public static void main(String[] args) {
+		System.out.println("Faid Hossain");
+		System.out.println();
+
+	}
+}
+
+//practice new
