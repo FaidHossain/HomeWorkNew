ba89dd4336efbe83d62aca5ae275a11234676ee0
FaidHossain <fdhssain@gmail.com>
2022-01-26 20:17:40 -0500

    01262022 8:17pm

---
+++ src/hw6Q2IfElseCondition/Voter01.java
@@ -0,0 +1,11 @@
+package hw6Q2IfElseCondition;
+
+public class Voter01 {
+	public static void main(String[] args) {
+		int age = 18;
+
+		if (age < 18) {
+			System.out.println("i am not a voter");
+		}
+	}
+}
---
+++ src/hw6Q2IfElseCondition/Voter04.java
@@ -0,0 +1,11 @@
+package hw6Q2IfElseCondition;
+
+public class Voter04 {
+
+	public static void main(String[] args) {
+		// d) And finally the outcome will be "Please add a valid age". In the above 4 condition, use appropriate key word like if, else if, else to execute that you are a voter or not. Please organize the code (very important). don't give any white space. push the code to GitHub and add the link below.
+
+
+	}
+
+}
