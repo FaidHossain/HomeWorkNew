6f54e1749d18c1b7f2c7d4b86f8a040504e31790
FaidHossain <fdhssain@gmail.com>
2022-01-27 23:39:35 -0500

    01272022 11:39pm

--- src/hw01/MySelf.java
+++ src/hw01/MySelf.java
@@ -9,4 +9,4 @@ public class MySelf {
 	}
 }
 
-//practice new
+
--- src/hw6Q2IfElseCondition/Voter01.java
+++ src/hw6Q2IfElseCondition/Voter01.java
@@ -4,8 +4,13 @@ public class Voter01 {
 	public static void main(String[] args) {
 		int age = 18;
 
-		if (age < 18) {
-			System.out.println("i am not a voter");
+		if (age == 24) {
+			System.out.println(" I am a voter ");
+		} else if (age < 18) {
+			System.out.println(" I am a voter ");
+		} else if (age >= 18) {
+			System.out.println(" I am not a voter ");
+			System.out.println(" Plese add a valid age ");
 		}
 	}
 }
