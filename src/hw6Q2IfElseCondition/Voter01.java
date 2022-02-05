d8622a622289fba737a1bdfb9241dd8915acdb14
FaidHossain <fdhssain@gmail.com>
2022-02-05 00:37:57 -0500

    02052022 12:37am

--- src/hw6Q2IfElseCondition/Voter01.java
+++ src/hw6Q2IfElseCondition/Voter01.java
@@ -3,14 +3,14 @@ package hw6Q2IfElseCondition;
 public class Voter01 {
 	public static void main(String[] args) {
 		int age = 18;
-
-		if (age == 24) {
-			System.out.println(" I am a voter ");
+		if (age == 18) {
+			System.out.println("I am a voter");
 		} else if (age < 18) {
-			System.out.println(" I am a voter ");
+			System.out.println("I am a voter");
 		} else if (age >= 18) {
-			System.out.println(" I am not a voter ");
-			System.out.println(" Plese add a valid age ");
+			System.out.println("I am not a voter");
+		} else {
+			System.out.println("Plese add a valid age");
 		}
 	}
 }
